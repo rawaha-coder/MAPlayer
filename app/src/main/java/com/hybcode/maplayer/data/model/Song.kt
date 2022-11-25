5349cecd620315fe260a3b19ffcd59634d4f44cb
rawaha-coder <rawaha.coder@gmail.com>
2022-11-25 07:05:13 +0100

    Create Song data class

---
+++ app/src/main/java/com/hybcode/maplayer/data/model/Song.kt
@@ -0,0 +1,20 @@
+package com.hybcode.maplayer.data.model
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity(tableName = "music_table")
+data class Song(
+    @PrimaryKey val songID: Long,
+    @ColumnInfo(name = "song_track") var track: Int,
+    @ColumnInfo(name = "song_title") var title: String,
+    @ColumnInfo(name = "song_artist") var artist: String,
+    @ColumnInfo(name = "song_album") var album: String,
+    @ColumnInfo(name = "song_album_id") val albumID: String,
+    @ColumnInfo(name = "song_uri") val uri: String,
+    @ColumnInfo(name = "song_year") var year: String
+) : Parcelable
