216f9b12b14d83c76ca30c05f2b3d3abd11eb9be
rawaha-coder <rawaha.coder@gmail.com>
2022-11-25 07:53:37 +0100

    Create room database and dao

---
+++ app/src/main/java/com/hybcode/maplayer/data/db/MusicDatabase.kt
@@ -0,0 +1,24 @@
+package com.hybcode.maplayer.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.hybcode.maplayer.data.model.Song
+
+@Database(entities = [Song::class], version = 1, exportSchema = false)
+abstract class MusicDatabase: RoomDatabase() {
+    abstract fun musicDao(): MusicDao
+    companion object {
+        @Volatile
+        private var database: MusicDatabase? = null
+        fun getDatabase(context: Context): MusicDatabase {
+            database ?: kotlin.run {
+                database = Room
+                    .databaseBuilder(context, MusicDatabase::class.java, "music_database")
+                    .build()
+            }
+            return database!!
+        }
+    }
+}
