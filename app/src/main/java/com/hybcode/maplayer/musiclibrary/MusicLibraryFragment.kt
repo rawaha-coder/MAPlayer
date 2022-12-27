ca51ca6f283644dc26471e776c563cf4452af87e
rawaha-coder <rawaha.coder@gmail.com>
2022-12-27 23:35:29 +0100

    Create music library package

---
+++ app/src/main/java/com/hybcode/maplayer/musiclibrary/MusicLibraryFragment.kt
@@ -0,0 +1,32 @@
+package com.hybcode.maplayer.musiclibrary
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hybcode.maplayer.R
+
+class MusicLibraryFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MusicLibraryFragment()
+    }
+
+    private lateinit var viewModel: MusicLibraryViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_music_library, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MusicLibraryViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/hybcode/maplayer/musiclibrary/MusicLibraryViewModel.kt
@@ -0,0 +1,7 @@
+package com.hybcode.maplayer.musiclibrary
+
+import androidx.lifecycle.ViewModel
+
+class MusicLibraryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
