5246b59204fbee6a08a3fd900661c4dbbed87770
rawaha-coder <rawaha.coder@gmail.com>
2022-12-29 20:09:57 +0100

    override onRequestPermissionResult

--- app/src/main/java/com/hybcode/maplayer/common/MainActivity.kt
+++ app/src/main/java/com/hybcode/maplayer/common/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.hybcode.maplayer.common
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.AudioManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.hybcode.maplayer.R
 import com.hybcode.maplayer.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,5 +21,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            //libraryMaintenance()
+        } else ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            1)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            //libraryMaintenance()
+        } else {
+            Toast.makeText(this,
+                getString(R.string.permission_required),
+                Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 }
