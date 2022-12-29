54e1f663e8f90711970997cf6c78b4e99e38bb41
rawaha-coder <rawaha.coder@gmail.com>
2022-12-29 21:06:15 +0100

    Create playback fragment

--- app/src/main/java/com/hybcode/maplayer/musiclibrary/MusicLibraryFragment.kt
+++ app/src/main/java/com/hybcode/maplayer/musiclibrary/MusicLibraryFragment.kt
@@ -1,21 +1,32 @@
 package com.hybcode.maplayer.musiclibrary
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hybcode.maplayer.R
+import androidx.core.view.isVisible
+import com.hybcode.maplayer.databinding.FragmentMusicLibraryBinding
 
 class MusicLibraryFragment : Fragment() {
+
+    private var _binding : FragmentMusicLibraryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_music_library, container, false)
+    ): View {
+        _binding = FragmentMusicLibraryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        updateScreenState()
+    }
 
+    private fun updateScreenState() {
+        binding.progressBar.isVisible = true
+    }
 }
